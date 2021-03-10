1c3763f522164fe547384191a0573ddcfdf5bf05
17476 <1341951524@qq.com>
2021-03-10 13:38:46 +0800

    1.备注:初始化;

---
+++ HJ001/HJ001/Startup.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HJ001
+{
+    public class Startup
+    {
+        /// <summary>
+        /// 服务与.Net Core拿到各种配置文件
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            #region 中间件
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Run");
+            //});
+
+            ////过渡中间件
+            //app.Use(async (context, next) =>
+            //{
+            //    //中文化
+            //    context.Response.ContentType = "text/plain;charset=utf-8";
+
+            //    logger.LogInformation("M1:传入请求");
+            //    await next();
+            //    logger.LogInformation("M1:传出响应");
+            //});
+            #endregion
+
+            #region 两种静态文件的中间件用法
+
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("htmlpage.html");
+            app.UseFileServer(fileServerOptions);
+
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("htmlpage.html");
+
+
+            ////添加默认文件
+            //app.UseDefaultFiles(defaultFilesOptions);
+
+            ////添加静态文件，添加后才可以访问wwwroot里面的文件
+            //app.UseStaticFiles();
+
+            #endregion
+
+            app.UsePathBase();
+
+            //终端中间件一般只能有一个
+            app.Run(async (context) =>
+            {
+                logger.LogInformation("M3");
+                //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                var configValue = _configuration["MyKey"];
+                await context.Response.WriteAsync(configValue);
+            });
+        }
+    }
+}
